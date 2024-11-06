18ebbccd144ea0aadcb67cafad0d5eb9715f9089
e-nihilus <epardorayo@gmail.com>
2024-11-06 17:10:21 +0100

    creacion pagina libros

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -13,8 +13,8 @@ export class BooksComponent {
   constructor() {
     this.books = [
       new Book("El Señor de los Anillos", "blanda", "J.R.R. Tolkien", 45.99, "https://th.bing.com/th/id/OIP.09ewS42LCUHAmk8zt-ETUQHaK5?w=193&h=284&c=7&r=0&o=5&pid=1.7"),
-      //new Book("Viaje al Centro de la Tierra", "balnda", "Julio Verne", 19.99, "https://th.bing.com/th/id/OIP.hfKiqqxjHlLn09SByMiMBwHaKN?w=195&h=270&c=7&r=0&o=5&pid=1.7"),
-      new Book("Dune", "balnda", "Frank Herbert", 29.99, "https://th.bing.com/th/id/OIP.cvoSdGRO8TtT-zw5N0qAAQHaLL?w=195&h=295&c=7&r=0&o=5&pid=1.7"),
+      //new Book("Viaje al Centro de la Tierra", "blanda", "Julio Verne", 19.99, "https://th.bing.com/th/id/OIP.hfKiqqxjHlLn09SByMiMBwHaKN?w=195&h=270&c=7&r=0&o=5&pid=1.7"),
+      new Book("Dune", "blanda", "Frank Herbert", 29.99, "https://th.bing.com/th/id/OIP.cvoSdGRO8TtT-zw5N0qAAQHaLL?w=195&h=295&c=7&r=0&o=5&pid=1.7"),
       new Book("En las Montañas de la Locura", "dura", "H.P. Lovecraft", 17.99, "https://th.bing.com/th/id/OIP._pQnuIhVJPq4C-GN7eqghAHaLL?w=195&h=295&c=7&r=0&o=5&pid=1.7"),
     ];
   }
