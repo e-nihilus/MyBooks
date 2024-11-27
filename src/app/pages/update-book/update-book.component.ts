653e857b18e7cb52464490a64885943730141a58
e-nihilus <epardorayo@gmail.com>
2024-11-27 10:11:39 +0100

    error en modificacion de libros

--- src/app/pages/update-book/update-book.component.ts
+++ src/app/pages/update-book/update-book.component.ts
@@ -3,41 +3,41 @@ import { Book } from 'src/app/models/book';
 import { BooksService } from 'src/app/shared/books.service';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-import { Respuesta } from 'src/app/models/respuesta'; 
+import { Respuesta } from 'src/app/models/respuesta';
 
 @Component({
   selector: 'app-update-book',
   templateUrl: './update-book.component.html',
-  styleUrls: ['./update-book.component.css']
+  styleUrls: ['./update-book.component.css'],
 })
-export class UpdateBookComponent { 
-  
+export class UpdateBookComponent {
+  public book: Book = new Book('', '', '', null, '', null);
 
-  public book: Book = new Book('', '', '', null, '', null); 
+  constructor(
+    private booksService: BooksService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
-  constructor(private booksService: BooksService, private router: Router, private toastr: ToastrService) {}
-
-  public modBook(title: string, type: string, author: string, price: number, photo: string, id_book: number): void {
-    this.book = new Book(title, type, author, price, photo, id_book);
+  public modBook(): void {
 
     this.booksService.edit(this.book).subscribe(
-      (response: Respuesta) => { 
-        if (!response.error) { 
-          this.toastr.success("Libro editado satisfactoriamente", "Exito", {
-            timeOut: 2000, positionClass: 'toast-top-center'
+      (response: Respuesta) => {
+        console.log('Respuesta del backend:', response);
+        if (!response.error) {
+          this.toastr.success('Libro editado satisfactoriamente', 'Éxito', {
+            timeOut: 2000,
+            positionClass: 'toast-top-center',
           });
           this.router.navigate(['/books']);
-          this.book = new Book('', '', '', 0, '', 0); 
+          this.book = new Book('', '', '', null, '', null); 
         } else {
-          this.toastr.error("El Ref no existe", "Error", {
-            timeOut: 2000, positionClass: 'toast-top-center'
-          });
+          this.toastr.error('No se pudo editar el libro', 'Error');
         }
       },
-      error => {
-        this.toastr.error("Error en la conexión", "Error", {
-          timeOut: 2000, positionClass: 'toast-top-center'
-        });
+      (error) => {
+        console.error('Error en la conexión:', error);
+        this.toastr.error('Error en la conexión', 'Error');
       }
     );
   }
